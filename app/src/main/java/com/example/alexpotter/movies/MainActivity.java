a6ed77c77dfbfbf6589f63acda649e33619a700d
Alex Potter <alex.potter1993@gmail.com>
2016-02-28 22:12:44 +0000

    Get movie data pulling into app

--- app/src/main/java/com/example/alexpotter/movies/MainActivity.java
+++ app/src/main/java/com/example/alexpotter/movies/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.alexpotter.movies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
+import org.json.JSONException;
 import org.json.JSONObject;
-
+import java.io.IOException;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -17,30 +18,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OkHttpClient client = new OkHttpClient();
+        new MyAsyncTask().execute();
+    }
 
-        HttpUrl url = new HttpUrl.Builder()
-                .scheme("http")
-                .host("www.omdbapi.com")
-                .addQueryParameter("t", "Frozen")
-                .build();
+    class MyAsyncTask extends AsyncTask<Void, Void, String> {
 
-        Request request = new Request.Builder()
-                .url(url.toString())
-                .build();
+        @Override
+        protected String doInBackground(Void... params) {
+            OkHttpClient client = new OkHttpClient();
 
-        Log.i("DEBUG", url.toString());
+            HttpUrl url = new HttpUrl.Builder()
+                    .scheme("http")
+                    .host("www.omdbapi.com")
+                    .addQueryParameter("t", "Inception")
+                    .build();
 
-        try {
-            Response response = client.newCall(request).execute();
-            String responseData = response.body().string();
-            JSONObject json = new JSONObject(responseData);
-            final String title = json.getString("Title");
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
 
-            Log.i("DEBUG", title);
+            Log.i("DEBUG", url.toString());
+
+            try {
+                Response response = client.newCall(request).execute();
+                String responseData = response.body().string();
+
+                return responseData;
+            }
+            catch (IOException e) {
+
+            }
+            catch (Exception e) {
+
+            }
+            return null;
         }
-        catch (Exception e) {
-            Log.i("DEBUG", e.getMessage());
+
+        @Override
+        protected void onPostExecute(String results) {
+            if (results != null) {
+                try {
+                    JSONObject json = new JSONObject(results);
+                    final String title = json.getString("Title");
+                    final String year = json.getString("Year");
+                    final String plot = json.getString("Plot");
+
+                    Log.d("DEBUG", title);
+                    Log.d("DEBUG", year);
+                    Log.d("DEBUG", plot);
+                }
+                catch (JSONException e) {
+
+                }
+            }
+            else
+            {
+                // Catch no movies found;
+            }
         }
     }
 }
